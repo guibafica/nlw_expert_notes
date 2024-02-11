d2452192617ffdb1105a5f46e83bb87b804455ad
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-02-10 22:53:18 -0500

    Adjust responsivity

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -104,7 +104,7 @@ export function NewNoteCard({ onNoteCreated }: INewNoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/60" />
 
-        <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[60vh] max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="overflow-hidden fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:h-[60vh] md:max-w-[640px] w-full bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100 transition-all">
             <X className="size-5" />
           </Dialog.Close>
